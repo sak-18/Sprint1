9586230c9cfccfae0a87f04dde17f071e917778d
Saketh Vishnubhatla <saketh181199@gmail.com>
2020-02-23 12:58:53 +0530

    Linked profile page to register and login page

---
+++ src/views/examples/RegisterPage.js
@@ -0,0 +1,185 @@
+import React from "react";
+
+// reactstrap components
+import {
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Col
+} from "reactstrap";
+
+// core components
+import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
+import TransparentFooter from "components/Footers/TransparentFooter.js";
+//import SignUp from "../index-sections/SignUp.js";
+
+function RegisterPage() {
+  const [firstFocus, setFirstFocus] = React.useState(false);
+  const [lastFocus, setLastFocus] = React.useState(false);
+  const [emailFocus, setEmailFocus] = React.useState(false);
+  const [dropdownOpen, setOpen] = React.useState(false);
+
+  const toggle = () => setOpen(!dropdownOpen);
+  React.useEffect(() => {
+    document.body.classList.add("login-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("login-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <ExamplesNavbar />
+      <div className="page-header clear-filter" filter-color="blue">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: "url(" + require("assets/img/login.jpg") + ")"
+          }}
+        ></div>
+        <div className="content">
+          <Container>
+            <Col className="ml-auto mr-auto" md="4">
+              <Card className="card-login card-plain">
+                <Form action="" className="form" method="">
+                  <CardHeader className="text-center">
+                    <CardTitle className="title-up" tag="h3">
+                      Sign Up
+                    </CardTitle>
+                    <div className="social-line">
+                      <Button
+                        className="btn-neutral btn-icon btn-round"
+                        color="facebook"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <i className="fab fa-facebook-square"></i>
+                      </Button>
+
+                      <Button
+                        className="btn-neutral btn-icon btn-round"
+                        color="google"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="lg"
+                      >
+                        <img
+                          width="30px"
+                          style={{ marginTop: "15px", marginRight: "2px" }}
+                          alt="Google sign-in"
+                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"
+                        />
+                      </Button>
+
+                      <Button
+                        className="btn-neutral btn-icon btn-round"
+                        color="twitter"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <i className="fab fa-twitter"></i>
+                      </Button>
+                    </div>
+                  </CardHeader>
+                  <CardBody>
+                    <InputGroup
+                      className={
+                        "no-border" + (firstFocus ? " input-group-focus" : "")
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons users_circle-08"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Name"
+                        type="text"
+                        onFocus={() => setFirstFocus(true)}
+                        onBlur={() => setFirstFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                    <InputGroup
+                      className={
+                        "no-border" + (lastFocus ? " input-group-focus" : "")
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons text_caps-small"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="ID Number"
+                        type="text"
+                        onFocus={() => setLastFocus(true)}
+                        onBlur={() => setLastFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                    <InputGroup
+                      className={
+                        "no-border" + (emailFocus ? " input-group-focus" : "")
+                      }
+                    >
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="now-ui-icons ui-1_email-85"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input
+                        placeholder="Email..."
+                        type="text"
+                        onFocus={() => setEmailFocus(true)}
+                        onBlur={() => setEmailFocus(false)}
+                      ></Input>
+                    </InputGroup>
+                  </CardBody>
+                  <CardFooter className="text-center">
+                    <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+                      <DropdownToggle caret>Button Dropdown</DropdownToggle>
+                      <DropdownMenu>
+                        <DropdownItem header>Header</DropdownItem>
+                        <DropdownItem disabled>Action</DropdownItem>
+                        <DropdownItem>Another Action</DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>Another Action</DropdownItem>
+                      </DropdownMenu>
+                    </ButtonDropdown>
+
+                    <Button
+                      className="btn-neutral btn-round"
+                      color="info"
+                      href="/profile-page"
+                      size="lg"
+                    >
+                      Get Started
+                    </Button>
+                  </CardFooter>
+                </Form>
+              </Card>
+            </Col>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RegisterPage;
