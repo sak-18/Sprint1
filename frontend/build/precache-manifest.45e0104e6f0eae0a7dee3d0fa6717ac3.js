4573dc41969cf83bd2447df7e0c2a26ecc3caad1
Saketh Vishnubhatla <saketh181199@gmail.com>
2020-04-08 16:44:33 +0530

    made changes to heroku postbuild

---
+++ frontend/build/precache-manifest.45e0104e6f0eae0a7dee3d0fa6717ac3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedd7444312c59adf0d47fed9648c6c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11c13c3e2c9ca41571a",
+    "url": "/static/css/main.94d1829e.chunk.css"
+  },
+  {
+    "revision": "e47a683863c810c40a84",
+    "url": "/static/js/2.f8dd608d.chunk.js"
+  },
+  {
+    "revision": "0fcf57b891adb4f62ea268dfa47311ed",
+    "url": "/static/js/2.f8dd608d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11c13c3e2c9ca41571a",
+    "url": "/static/js/main.f5e2f638.chunk.js"
+  },
+  {
+    "revision": "97a7e0dfd729b6ad4cd0",
+    "url": "/static/js/runtime-main.f11285d9.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/static/media/ryan.f217f346.jpg"
+  }
+]);
